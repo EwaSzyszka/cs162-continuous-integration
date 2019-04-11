d61154794f17239e8ef876d313df4a6858f4a4d1
EwaSzyszka <ewa.szyszka@minerva.kgi.edu>
2019-04-11 08:05:22 -0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -14,36 +14,6 @@ class DockerComposeTestCase(unittest.TestCase):
         result = requests.post('http://localhost:5000/add', data=incorrect)
         self.assertNotEqual(result.status_code, 200)
 
-    def test_db(self):
-        expression = {'expression': "3+2"}
-        result = requests.post('http://localhost:5000/add', data=expression)
-        engine = create_engine('postgresql://cs162_user:cs162_password@localhost:5432/cs162', echo = True)
-
-        connection = engine.connect()
-        results = connection.execute("SELECT * FROM Expression WHERE text='3+2'").fetchall()
-
-        self.assertNotEqual(len(results), 0)
-
-    def test_correct_result(self):
-        expression = {'expression': "3+2"}
-        result = requests.post('http://localhost:5000/add', data=expression)
-        engine = create_engine('postgresql://cs162_user:cs162_password@localhost:5432/cs162', echo = True)
-
-        connection = engine.connect()
-        results = connection.execute("SELECT value FROM Expression WHERE text='3+2'").first()
-
-        self.assertEqual(results[0], 5.0)
-
-    def test_error_db(self):
-        incorrect = {"expression": "Hello xDD"}
-        result = requests.post('http://localhost:5000/add', data=incorrect)
-        engine = create_engine('postgresql://cs162_user:cs162_password@localhost:5432/cs162', echo = True)
-
-        connection = engine.connect()
-        results = connection.execute("SELECT * FROM Expression WHERE text='Hello xDD'").fetchall()
-
-        self.assertEqual(len(results), 0)
-
 
 
 if __name__ == '__main__':
